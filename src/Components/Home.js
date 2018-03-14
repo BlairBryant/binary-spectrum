84c2964bbafc01b2d6b1f45c62f3285379b407ed
Blair <blairslc@gmail.com>
2018-03-14 17:07:44 -0600

    Styled Home screen and started styling auth0 page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,12 +5,15 @@ class Home extends Component {
     render() {
         return (
             <div className="home">
-                    <p className='topLeftLine'></p>
-                    <h1 className='binary'>Binary</h1>
-                    <h1 className='spectrum'>Spectrum</h1>
-                    <a href='http://192.168.1.81:3500/auth' className='signInButton'><div className='signInDiv'>
-                    Sign in
-                    </div></a>
+                <div className='colorTop'></div>
+                <div className='colorBottom'></div>
+                <p className='topLeftLine'></p>
+                <h1 className='binary'>Binary</h1>
+                <h1 className='spectrum'>Spectrum</h1>
+                <h2>Discover where you stand on various fronts</h2>
+                <a href='http://192.168.1.81:3500/auth' className='signInButton'><div className='signInDiv'>
+                Sign in
+                </div></a>
             </div>
         )
     }
